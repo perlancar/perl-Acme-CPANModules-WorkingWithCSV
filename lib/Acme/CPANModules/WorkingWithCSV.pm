d2266ec1af5f3771fc19be22ebe1dc19d6fd7624
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2022-07-27 09:09:39 +0700

    Release v0.001

--- lib/Acme/CPANModules/WorkingWithCSV.pm
+++ lib/Acme/CPANModules/WorkingWithCSV.pm
@@ -1,82 +1,132 @@
-package Acme::CPANModules::WorkingWithTree;
+package Acme::CPANModules::WorkingWithCSV;
+
+use strict;
+use Acme::CPANModulesUtil::Misc;
 
 # AUTHORITY
 # DATE
 # DIST
 # VERSION
 
-use strict;
-use Acme::CPANModulesUtil::Misc;
-
 my $text = <<'_';
-**Basic modules**
+The following are tools (modules and scripts) to work with the CSV format:
+
+
+**Parsing**
 
 First things first, the most important module to work with CSV in Perl is
 <pm:Text::CSV> (which will use <pm:Text::CSV_XS> backend when possible but fall
 back to <pm:Text::CSV_PP> otherwise). It's not in core, but only a cpanm command
 away.
 
-Below are other modules, tools, and recipes related to CSV, organized by task.
 
-Keyword: CSV, TSV, comma-separated
+**Generating CSV from Perl array/structure**
+
+<pm:Text::CSV> (as well as <pm:Text::CSV_XS>) can render a line of CSV from Perl
+array(ref) with their `say()` method.
+
+<prog:dd2csv> from <pm:App::CSVUtils>
+
+<pm:Perinci::CmdLine> framework can render function result (CLI output) as CSV.
+
+
+**Converting to/from other formats**
+
+*INI*: <prog:ini2csv> from <pm:App::TextTableUtils>
+
+*TSV*: <prog:csv2tsv> and L<prog:tsv2csv> from <pm:App::CSVUtils>
+
+*LTSV*: <prog:csv2ltsv> from <pm:App::CSVUtils> and L<prog:ltsv2csv> from
+<pm:App::LTSVUtils>
+
+*XLS* and *XLSX*: <prog:csv2tsv> and <prog:tsv2csv> from <pm:App::CSVUtils>
+
+*JSON*: <prog:csv2json> and <prog:json2csv> from <pm:App::TextTableUtils>
+
+*Markdown table*: <prog:csv2mdtable> from <pm:App::TextTableUtils>
+
+*Org table*: <prog:csv2orgtable> from <pm:App::TextTableUtils>
+
+*SQLite database*: <prog:csv2sqlite> from <pm:App::SQLiteUtils>
+
+
+**Rendering as text/ASCII table**
+
+<prog:csv2texttable> from <pm:App::TextTableUtils>
 
 
 **Changing field separator character, field quote character, and/or escape character**
 
-TODO
-
-See also: *Converting CSV to TSV*, *Converting TSV to CSV*.
+<prog:csv-csv> from <pm:App::CSVUtils>
 
 
-**Converting CSV to data structure**
+**Adding/removing columns**
 
-Using Text::CSV:
-
-    my $aoa = csv(in => "data.csv");
-
-CLI: <prog:csv-dump> from <pm:App::CSVUtils>
+<prog:csv-add-field>, <prog:csv-delete-field>, <prog:csv-select-fields> from
+<pm:App::CSVUtils>
 
 
-**Converting CSV to TSV**
+**Processing columns of CSV with Perl code**
 
-TODO
+<prog:csv-munge-field> from <pm:App::CSVUtils>
 
 
-**Converting TSV to CSV**
+**Processing rows of CSV with Perl code**
 
-TODO
+Aside from the obvious <pm:Text::CSV>, you can also use <prog:csv-each-row>,
+<prog:csv-munge-row>, <prog:csv-mp> from <pm:App::CSVUtils>.
+
+
+**Merging several CSV files into one**
+
+<prog:csv-concat> from <pm:App::CSVUtils>
+
+
+**Splitting a CSV file into several**
+
+<prog:csv-split> from <pm:App::CSVUtils>
+
+
+**Sorting CSV rows**
+
+<prog:csv-sort> from <pm:App::CSVUtils>
+
+
+**Sorting CSV columns**
+
+<prog:csv-sort-fields> from <pm:App::CSVUtils>
 
 
 **Filtering CSV columns**
 
-Problem: you only want to pick some CSV columns.
-
-TODO
+<prog:csv-select-fields> from <pm:App::CSVUtils>
 
 
 **Filtering CSV rows**
 
-Problem: you only want to pick some CSV rows based on some criteria.
+<prog:csv-grep> and <prog:csv-select-rows> from <pm:App::CSVUtils>
 
-Using grep: If the pattern is simple enough, then the Unix command `grep` and
-its friends can be used. If your CSV has header, you can do this to make sure
-the header row is always returned:
+<prog:csvgrep> from <pm:csvgrep>
 
-    % head -n1 YOUR.CSV; tail -n +2 YOUR.CSV | grep PATTERN
-
-Using csvgrep CLI: <prog:csvgrep> (from <pm:csvgrep>) allows you grep certain
-columns only and/or output certain columns only. It displays the result in a
-text table though.
-
-Using csv-grep CLI: <prog:csv-grep> (from <pm:App::CSVUtils>) works more like
-Perl's grep. It allows you to specify a Perl code which is presented the current
-CSV row as an arrayref (or hashref, if you use `-H`) and expected to return
-a boolean value to determine whether that row should be included or not.
 
 **Transposing CSV**
 
-TODO
+<prog:csv-transpose> from <pm:App::CSVUtils>
 
+
+**Summing and averaging rows**
+
+<prog:csv-sum> and <prog:csv-avg> from <pm:App::CSVUtils>
+
+
+**Producing frequency table from CSV**
+
+<prog:csv-freqtable> from <pm:App::CSVUtils>
+
+
+**Performing set operations (intersection, union, difference) on CSV**
+
+<prog:csv-setop> from <pm:App::CSVUtils>
 
 _
 
@@ -91,6 +141,8 @@ Acme::CPANModulesUtil::Misc::populate_entries_from_module_links_in_description;
 1;
 # ABSTRACT:
 
-=head1 SEE ALSO
+=head1 prepend:SEE ALSO
 
 L<App::CSVUtils::Manual::Cookbook>
+
+The See Also section in L<App::CSVUtils> documentation
